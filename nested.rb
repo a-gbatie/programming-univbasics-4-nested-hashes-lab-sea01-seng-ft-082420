bb9075ed894b3894cd2af10dc74e6901ea488932
Ange Batie <ange.batie@gmail.com>
2020-08-12 05:30:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,9 @@ def adding_matz
       }
     }
       programmer_hash[:yukihiro_matsumoto] = {}
-      programmer_hash[:yukihiro_matsumoto]
+      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby" 
+      programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+      puts programmer_hash
 end
 
 def changing_alan
@@ -104,7 +106,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+      programmer_hash[:alan_kay][:known_for] = "GUI"
+      puts programmer_hash
 end
 
 def adding_to_dennis
